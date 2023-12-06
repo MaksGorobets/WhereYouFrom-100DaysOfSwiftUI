6947ba88b4268dbe6bca240c03990a1a26ace46f
MaksGorobets <gorobets542@gmail.com>
2023-12-06 23:36:21 +0200

    Initial Commit

---
+++ WhereYouFrom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhereYouFrom
+//
+//  Created by Maks Winters on 06.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhereYouFrom/WhereYouFromApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhereYouFromApp.swift
+//  WhereYouFrom
+//
+//  Created by Maks Winters on 06.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct WhereYouFromApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
